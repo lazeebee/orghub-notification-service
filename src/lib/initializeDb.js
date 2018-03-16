d77d4d296bb6783e9ff8fbc446e632fe2781f7a0
Olle Lauri Boström <ollebostr@gmail.com>
2018-03-16 10:35:10 +0100

    Remove logging of db-adress to prevent leaking password

--- src/lib/initializeDb.js
+++ src/lib/initializeDb.js
@@ -5,7 +5,7 @@ export default function ({ dbAddress }, cb) {
     mongoose.Promise = global.Promise;
     mongoose.connect(dbAddress);
     mongoose.connection.on('error', error => console.log(error));
-    mongoose.connection.once('open', () => console.log(`connected to database at ${dbAddress}`));
+    mongoose.connection.once('open', () => console.log('connected to database at mlab.com'));
   }
 
   return cb();
